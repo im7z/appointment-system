c0f052c5c1064ff3b41f4c456ecbc7c226a0b6dc
im7z <xx7moodyxx7@gmail.com>
2025-11-18 00:15:50 +0300

    reminder msg adding

--- seedmsg.js
+++ seedmsg.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const fs = require("fs");
 const csv = require("csv-parser");
 
-mongoose.connect("mongodb://127.0.0.1:27017/appointments")
+const MONGO_URL = process.env.MONGO_URL;
+mongoose.connect(MONGO_URL)
   .then(() => console.log("✅ Connected to MongoDB"))
   .catch((err) => console.error("❌ Connection error:", err));
 
